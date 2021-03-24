0f9c64216dec48feb186da978ee1171e6cbc1dc1
Bodil74 <61984150+Bodil74@users.noreply.github.com>
2021-03-24 11:09:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ rubix.addFaces([[n, n + 16, n + 20, n + 4] for n in (0, 4, 8, 16, 20, 24, 32, 36
 rubix.addFaces([[n, n + 16, n + 17, n + 1] for n in (12, 13, 14, 28, 29, 30, 44, 45, 46, 0, 1, 2, 16, 17, 18, 32, 33, 34)])
 rubix.addFaces([[n, n + 1, n + 5, n + 4] for n in (0, 4, 8, 1, 5, 9, 2, 6, 10, 48, 52, 56, 49, 53, 57, 50, 54, 58)])
 
+#rubix.addColors((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for n in range(54))
+
 rubix.addColors([(255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0), (255, 0, 0),
 				(255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0), (255, 165, 0),
 				(0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0), (0, 255, 0),
@@ -53,19 +55,43 @@ class Help:
 						opened = False
 
 			pygame.draw.rect(pv.screen, (48, 48, 48), menu_rect)
-			pv.screen.blit(menu_text, (100, 50))
+			pv.screen.blit(menu_text, (100, 100))
 			pygame.draw.rect(pv.screen, (255, 255, 255), menu_rect, 1)
 
 			pygame.display.flip()
 
 
-def rotate_side():
-	pass
+class Timer():
+	def __init__(self):
+		pass
 
-def rotate_middle():
-	
-	for i in (1, 4, 7, 37, 40, 43, 16, 13, 10, 50, 49, 48):
-		rubix.colors[i] = (48, 48, 48)
+
+def rotate_side(side, color_copy):
+	cells = [i for i in range(side, side + 9)]
+	twod_cells = [[cells[n], cells[n + 1], cells[n + 2]] for n in (0, 3, 6)]
+	rotated_cells = list(zip(*reversed(twod_cells)))
+
+	if side == 0 or side == 9:
+		x = twod_cells
+		twod_cells = rotated_cells
+		rotated_cells = x
+
+	for i in range(len(twod_cells)):
+		for j, k in enumerate(twod_cells[i]):
+			rubix.colors[k] = color_copy[rotated_cells[i][j]] 
+
+def rotate(cells, side = -1):
+	color_copy = rubix.colors.copy()
+	shifted_cells = list(cells)
+
+	for i in range(3):
+		shifted_cells.insert(0, shifted_cells.pop(-1))
+
+	for i, j in enumerate(shifted_cells):
+		rubix.colors[j] = color_copy[cells[i]]
+
+	if side > -1:
+		rotate_side(side, color_copy)
 
 def scramble():
 	pass
@@ -86,9 +112,6 @@ def main():
 	pv.translateAll([-15, 0, 0])
 	pv.translateAll([0, -10, 0])
 
-	rotate_middle()
-	print(rubix.colors)
-
 	running = True
 	key_to_function = {pygame.K_EQUALS : (lambda x : x.scaleAll(1.25)),
 					   pygame.K_MINUS : (lambda x : x.scaleAll(0.8)),
@@ -97,7 +120,16 @@ def main():
 					   pygame.K_a : (lambda x : x.rotateAll('Y', 0.1)),
 					   pygame.K_s : (lambda x : x.rotateAll('Y', -0.1)),
 					   pygame.K_z : (lambda x : x.rotateAll('Z', 0.1)),
-					   pygame.K_x : (lambda x : x.rotateAll('Z', -0.1)),}
+					   pygame.K_x : (lambda x : x.rotateAll('Z', -0.1)),
+					   pygame.K_1 : (lambda x : rotate((33, 30, 27, 36, 37, 38, 18, 21, 24, 47, 46, 45), 0)),
+					   pygame.K_2 : (lambda x : rotate((34, 31, 28, 39, 40, 41, 19, 22, 25, 50, 49, 48))),
+					   pygame.K_3 : (lambda x : rotate((35, 32, 29, 42, 43, 44, 20, 23, 26, 53, 52, 51), 9)),
+					   pygame.K_4 : (lambda x : rotate((2, 5, 8, 47, 50, 53, 17, 14, 11, 44, 41, 38), 18)),
+					   pygame.K_5 : (lambda x : rotate((1, 4, 7, 46, 49, 52, 16, 13, 10, 43, 40, 37))),
+					   pygame.K_6 : (lambda x : rotate((0, 3, 6, 45, 48, 51, 15, 12, 9, 42, 39, 36), 27)),
+					   pygame.K_7 : (lambda x : rotate((2, 1, 0, 27, 28, 29, 9, 10, 11, 20, 19, 18), 36)),
+					   pygame.K_8 : (lambda x : rotate((5, 4, 3, 30, 31, 32, 12, 13, 14, 27, 22, 21))),
+					   pygame.K_9 : (lambda x : rotate((8, 7, 6, 33, 34, 35, 15, 16, 17, 26, 28, 24), 45))}
 
 	while running:
 
